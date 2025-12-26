6fe7a3470b4a278f2dba9b68d897b02fe902e0b2
cn1095 <1095053105@qq.com>
2025-12-26 16:34:33 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2798,30 +2798,80 @@ func main() {
     flag.Parse()
     
     //打印帮助信息
-    if showHelp {
-       colorText := func(color int, message string) string {
-			return fmt.Sprintf("\x1b[1;%dm%s\x1b[0m", color, message)
-		}
-
-	// 使用固定宽度确保对齐  
-    fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-p"), colorText(34, "[端口号]"), "监听指定端口号")  
-    fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-d"), colorText(34, "[文件路径]"), "指定数据存放的目录路径，默认当前程序路径的./short_data文件夹")  
-    fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-db"), colorText(34, "[文件路径]"), "指定IP地址离线数据存放的目录路径，默认/tmp文件夹")  
-    fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-log"), colorText(34, "[文件路径]"), "启用日志，并指定日志存放的目录路径")  
-    fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-admin"), "", "启用管理员后台页面")  
-    fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-e"), colorText(34, "[邮箱地址]"), "指定邮箱地址，修改页面的邮箱地址")  
-    fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-u"), colorText(34, "[账户名]"), "指定管理页面的登陆账户名")  
-    fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-w"), colorText(34, "[密码]"), "指定管理页面的登陆密码")  
-    fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-daemon"), "", "以后台模式运行")  
-    fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-redis-addr"), colorText(34, "[地址:端口]"), "Redis服务器地址 (例如: localhost:6379)")  
-    fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-redis-user"), colorText(34, "[用户名]"), "Redis用户名 (可选)")  
-    fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-redis-pass"), colorText(34, "[密码]"), "Redis密码 (可选)")  
-    fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-redis-pre"), colorText(34, "[前缀]"), "Redis数据前缀，默认为short")  
-    fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-v"), "", "版本号")  
-    fmt.Printf("  %-20s %-15s %s\n", colorText(36, "-h"), "", "帮助信息")  
+    if showHelp {  
+    colorText := func(color int, message string) string {  
+        return fmt.Sprintf("\x1b[1;%dm%s\x1b[0m", color, message)  
+    }  
+  
+    fmt.Printf("\nUsage: \n\n")  
+      
+    // 定义参数信息  
+    type HelpItem struct {  
+        Param    string  
+        Value    string  
+        Desc     string  
+    }  
+      
+    helpItems := []HelpItem{  
+        {"-p", "[端口号]", "监听指定端口号"},  
+        {"-d", "[文件路径]", "指定数据存放的目录路径，默认当前程序路径的./short_data文件夹"},  
+        {"-db", "[文件路径]", "指定IP地址离线数据存放的目录路径，默认/tmp文件夹"},  
+        {"-log", "[文件路径]", "启用日志，并指定日志存放的目录路径"},  
+        {"-admin", "", "启用管理员后台页面"},  
+        {"-e", "[邮箱地址]", "指定邮箱地址，修改页面的邮箱地址"},  
+        {"-u", "[账户名]", "指定管理页面的登陆账户名"},  
+        {"-w", "[密码]", "指定管理页面的登陆密码"},  
+        {"-daemon", "", "以后台模式运行"},  
+        {"-redis-addr", "[地址:端口]", "Redis服务器地址 (例如: localhost:6379)"},  
+        {"-redis-user", "[用户名]", "Redis用户名 (可选)"},  
+        {"-redis-pass", "[密码]", "Redis密码 (可选)"},  
+        {"-redis-pre", "[前缀]", "Redis数据前缀，默认为short"},  
+        {"-v", "", "版本号"},  
+        {"-h", "", "帮助信息"},  
+    }  
+      
+    // 计算最大参数长度和最大值长度  
+    maxParamLen := 0  
+    maxValueLen := 0  
+    for _, item := range helpItems {  
+        if len(item.Param) > maxParamLen {  
+            maxParamLen = len(item.Param)  
+        }  
+        if len(item.Value) > maxValueLen {  
+            maxValueLen = len(item.Value)  
+        }  
+    }  
+      
+    // 设置最小列宽  
+    if maxParamLen < 12 {  
+        maxParamLen = 12  
+    }  
+    if maxValueLen < 12 {  
+        maxValueLen = 12  
+    }  
+      
+    // 格式化输出  
+    for _, item := range helpItems {  
+        paramCol := colorText(36, item.Param)  
+        valueCol := colorText(34, item.Value)  
+          
+        if item.Value == "" {  
+            // 没有参数值的情况，说明列直接对齐到参数列结束位置  
+            fmt.Printf("  %-*s %s\n",   
+                maxParamLen + maxValueLen + 2, // 参数列+值列的总宽度  
+                paramCol,   
+                item.Desc)  
+        } else {  
+            // 有参数值的情况  
+            fmt.Printf("  %-*s %-*s %s\n",   
+                maxParamLen, paramCol,   
+                maxValueLen, valueCol,   
+                item.Desc)  
+        }  
+    }  
       
     return  
-   }
+}
     
     //打印版本信息
     if showVersion {
